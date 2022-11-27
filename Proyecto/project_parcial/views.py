c4b4f6532352b33c50d7f7e5258d5ab25ad764bd
FabioTorresMarino <a20194608@pucp.edu.pe>
2022-11-27 18:24:00 -0500

    avance_4

--- Proyecto/project_parcial/views.py
+++ Proyecto/project_parcial/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
+lista_usuarios = [['Fabio','Torres','20'],['Cristiano','Ronaldo','37'],['Lionel','Messi','35']]
 
 def index(request):
     return HttpResponse('Esta es mi primera vista')
 
 def login(request):
-    lista_usuarios = [['Fabio','Torres','20'],['Cristiano','Ronaldo','37'],['Lionel','Messi','35']]
+    if request.method == 'POST':
+        nuevoUsuario = []
+        print('Hola se ha posteado información')
+        nombre = request.POST.get('nombreUsuario')
+        apellido = request.POST.get('apellidoUsuario')
+        edad = request.POST.get('edadUsuario')
+        print('El nombre del usuario es: ' + str(nombre))
+        print('El apellido del usuario es: ' + str(apellido))
+        print('La edad del usuario es: ' + str(edad))
+        nuevoUsuario.append(str(nombre))
+        nuevoUsuario.append(str(apellido))
+        nuevoUsuario.append(str(edad))
+        lista_usuarios.append(nuevoUsuario)
     return render(request,'project_parcial/login.html',{
         'usuarios': lista_usuarios,
     })
