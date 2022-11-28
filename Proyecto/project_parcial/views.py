bb0d1a7c69e71bbd6ece4e2a84766d6c32fc005d
FabioTorresMarino <a20194608@pucp.edu.pe>
2022-11-27 23:29:22 -0500

    avance_9

--- Proyecto/project_parcial/views.py
+++ Proyecto/project_parcial/views.py
@@ -12,15 +12,10 @@ def index(request):
 def login(request):
     if request.method == 'POST':
         nuevoUsuario = []
-        print('Hola se ha posteado información')
         nombre = request.POST.get('nombreUsuario')
         apellido = request.POST.get('apellidoUsuario')
         codigo_usuario = request.POST.get('codigo_usuario')
         clave = request.POST.get('clave')
-        print('El nombre del usuario es: ' + str(nombre))
-        print('El apellido del usuario es: ' + str(apellido))
-        print('El codigo de usuario es: ' + str(codigo_usuario))
-        print('La clave de usuario es: ' + str(clave))
         nuevoUsuario.append(str(nombre))
         nuevoUsuario.append(str(apellido))
         nuevoUsuario.append(str(codigo_usuario))
@@ -33,4 +28,20 @@ def dashboard(request):
     return render(request,'')
 def view_tareas(request):
     return  render(request,'')
+def register_user(request):
+    if request.method == 'POST':
+        nuevoUsuario = []
+        nombre = request.POST.get('nombreUsuario')
+        apellido = request.POST.get('apellidoUsuario')
+        codigo_usuario = request.POST.get('codigo_usuario')
+        clave = request.POST.get('clave')
+        nuevoUsuario.append(str(nombre))
+        nuevoUsuario.append(str(apellido))
+        nuevoUsuario.append(str(codigo_usuario))
+        nuevoUsuario.append(str(clave))
+        usuario(nombre=str(nombre),apellido=str(apellido),codigo_usuario=str(codigo_usuario),clave=str(clave)).save()
+    return render(request,'project_parcial/login.html',{
+        'usuarios': usuario.objects.all(),
+    })
+
 
