fabceb5399196dc7971135e121c05d49d6df7a4c
FabioTorresMarino <a20194608@pucp.edu.pe>
2022-11-27 23:52:30 -0500

    avance_10

--- Proyecto/project_parcial/urls.py
+++ Proyecto/project_parcial/urls.py
@@ -5,5 +5,8 @@ app_name = 'project_parcial'
 
 urlpatterns =[
     path('',views.index,name='index'),
-    path('login',views.login,name='login')
+    path('login',views.login,name='login'),
+    path('register_user',views.register_user,name='register_user'),
+    path('dashboard',views.dashboard,name='dashboard'),
+    path('view_tareas',views.view_tareas,name='view_tareas'),
 ]
--- Proyecto/project_parcial/views.py
+++ Proyecto/project_parcial/views.py
@@ -11,23 +11,19 @@ def index(request):
 
 def login(request):
     if request.method == 'POST':
-        nuevoUsuario = []
+        usuario_solicitante = []
         nombre = request.POST.get('nombreUsuario')
-        apellido = request.POST.get('apellidoUsuario')
-        codigo_usuario = request.POST.get('codigo_usuario')
         clave = request.POST.get('clave')
-        nuevoUsuario.append(str(nombre))
-        nuevoUsuario.append(str(apellido))
-        nuevoUsuario.append(str(codigo_usuario))
-        nuevoUsuario.append(str(clave))
-        usuario(nombre=str(nombre),apellido=str(apellido),codigo_usuario=str(codigo_usuario),clave=str(clave)).save()
+        usuario_solicitante.append(str(nombre))
+        usuario_solicitante.append(str(clave))
+        usuario(nombre=str(nombre),clave=str(clave)).save()
     return render(request,'project_parcial/login.html',{
         'usuarios': usuario.objects.all(),
     })
 def dashboard(request):
-    return render(request,'')
+    return render(request,'project_parcial/dashboard.html')
 def view_tareas(request):
-    return  render(request,'')
+    return  render(request,'project_parcial/view_tareas.html')
 def register_user(request):
     if request.method == 'POST':
         nuevoUsuario = []
@@ -40,7 +36,7 @@ def register_user(request):
         nuevoUsuario.append(str(codigo_usuario))
         nuevoUsuario.append(str(clave))
         usuario(nombre=str(nombre),apellido=str(apellido),codigo_usuario=str(codigo_usuario),clave=str(clave)).save()
-    return render(request,'project_parcial/login.html',{
+    return render(request,'project_parcial/register_user.html',{
         'usuarios': usuario.objects.all(),
     })
 
