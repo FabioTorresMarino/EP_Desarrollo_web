009bf78e3942e400456eb5238366f161b281f893
FabioTorresMarino <a20194608@pucp.edu.pe>
2022-11-27 22:35:08 -0500

    avance_7

--- Proyecto/project_parcial/views.py
+++ Proyecto/project_parcial/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import usuario
+from .models import tarea 
 
 # Create your views here.
 lista_usuarios = [['Fabio','Torres','20'],['Cristiano','Ronaldo','37'],['Lionel','Messi','35']]
@@ -13,19 +15,18 @@ def login(request):
         print('Hola se ha posteado información')
         nombre = request.POST.get('nombreUsuario')
         apellido = request.POST.get('apellidoUsuario')
-        edad = request.POST.get('edadUsuario')
+        codigoUsuario = request.POST.get('codigoUsuario')
         print('El nombre del usuario es: ' + str(nombre))
         print('El apellido del usuario es: ' + str(apellido))
-        print('La edad del usuario es: ' + str(edad))
+        print('El codigo de usuario es: ' + str(codigoUsuario))
         nuevoUsuario.append(str(nombre))
         nuevoUsuario.append(str(apellido))
-        nuevoUsuario.append(str(edad))
-        lista_usuarios.append(nuevoUsuario)
+        nuevoUsuario.append(str(codigoUsuario))
     return render(request,'project_parcial/login.html',{
-        'usuarios': lista_usuarios,
+        'usuarios': usuario.objects.all(),
     })
 def dashboard(request):
     return render(request,'')
 def view_tareas(request):
     return  render(request,'')
-    
+
